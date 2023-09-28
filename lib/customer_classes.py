ccd9fd3ba8545903859e53aabc33a6a181038eff
Ben <benjdixon96@yahoo.co.uk>
2023-09-28 13:08:12 +0100

    finished added twilio sms

--- lib/customer_classes.py
+++ lib/customer_classes.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from twilio.rest import Client
 
 from datetime import datetime
 
@@ -56,10 +57,10 @@ class Dish():
 #
 #   has a function place oder this will 
 #    
-#
+#     class customer, keeps a phomne number
 #   
 #   
-#
+#       
 # 
 #      
 roast_chicken = Dish("roast chicken", 11.50)
@@ -71,12 +72,19 @@ my_menu = [roast_chicken, burger, fries, onion_rings, milk_shake]
 
 
 
+########### these are obviously not my details but it does work ################################
+account_sid = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token  = "XXxxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXX" 
+
+
+
 
 
 class Order():
     def __init__(self, menu):
         self.menu = menu
-        self.order = [] 
+        self.order = []
+
     def show_menu(self):
         res = ""
         for i in self.menu:
@@ -124,7 +132,7 @@ class Order():
 
 
 class Order_tracker():
-    def __init__(self, request=None):
+    def __init__(self, request=None, my_phone_number="+44##########", rest_phone_number="44###########"):
         self.my_order = []
         self.total_for_order = 0
         eta = 0
@@ -133,19 +141,28 @@ class Order_tracker():
         self.request = request
         self.eta = 0
         self.oustanding_order = False
+        self.my_phone_number = my_phone_number
+        self.rest_phone_number = rest_phone_number
     def make_order(self, order, deliverytime=30):
         if self.my_order != []:
             self.receipts[f'order made on {self.order_time[0]} was made at {self.order_time[1]}'] = [self.show_order()[63:-47], f'should arrive at {self.eta}'[:-3]]
             self.order_time = []
             self.eta = 0
             self.my_order = []
-            
         self.my_order = order.order
         self.total_for_order = order.grand_total()
         self._get_order_time()
         self._get_eta(deliverytime)
         self.oustanding_order = False
-        return f'thank you your order it will arrive by {str(self.eta)[0:5]}' 
+        result = f'thank you your order it will arrive by {str(self.eta)[0:5]}'
+        this_client = Client(account_sid, auth_token)
+        message = this_client.messages.create(
+        to=self.my_phone_number,
+        from_=self.rest_phone_number,
+        body=result)
+        print(message.sid)
+        return result 
+
     def show_order(self):
         res = f'your most recent order was made at {self.order_time[1]} on {self.order_time[0]}: order: '
         total_cost = self.total_for_order
@@ -187,8 +204,6 @@ class Order_tracker():
             res += x[0]
             res += ' '
             res += x[1]
-            
-
         return res    
 
 
@@ -197,7 +212,6 @@ class Order_tracker():
 
 
 
-        
 
 
 
